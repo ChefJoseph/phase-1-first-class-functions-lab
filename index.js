d195d513b5457f9c67b94500756fc2040dec149e
Joseph Hirotsu <jhirotsu@gmail.com>
2022-06-12 01:17:45 -0400

    commit tree 6f8a587aed9e504b627d21ed2382e7e8f6bb2494

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+// Array.drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (drivers) => {
+   return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+const createFareMultiplier = (multiplyRate) => {
+    return function (fare) {
+        return multiplyRate*fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+  };
